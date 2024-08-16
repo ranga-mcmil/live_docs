4a2d3fb1ad171d919b64aa55ad23017e5866cbb0
Ranga McMil <mukanhairiranga@gmail.com>
2024-08-16 09:47:09 +0200

    editor page

--- app/(root)/documents/[id]/page.tsx
+++ app/(root)/documents/[id]/page.tsx
@@ -1,13 +1,16 @@
+import { Editor } from "@/components/editor/Editor"
+import Header from "@/components/Header"
 
 const Document = () => {
     return (
-      <div>
-        document id
-      </div>
+        <div>
+            <Header>
+                <p>Test</p>
+            </Header>
+            <Editor />
+        </div>
     )
-  }
-  
-  export default Document
-  
-  
-  
+}
+
+export default Document
+ 
