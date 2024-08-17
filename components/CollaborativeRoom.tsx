e592a46d4a7e5e044adb5c6b7d03e5a32959dc91
Ranga McMil <mukanhairiranga@gmail.com>
2024-08-17 18:57:52 +0200

    editing title

--- components/CollaborativeRoom.tsx
+++ components/CollaborativeRoom.tsx
@@ -1,41 +1,92 @@
 "use client"
 
-import { RoomProvider, ClientSideSuspense} from "@liveblocks/react/suspense";
+import { RoomProvider, ClientSideSuspense } from "@liveblocks/react/suspense";
 import { Editor } from "@/components/editor/Editor"
 import Header from "@/components/Header"
 import { SignInButton, SignedIn, SignedOut, UserButton } from '@clerk/nextjs'
 import ActiveCollaborators from "./ActiveCollaborators";
+import { useRef, useState } from "react";
+import { Input } from "./ui/input";
+import { currentUser } from "@clerk/nextjs/server";
+import Image from "next/image";
 
 const CollaborativeRoom = ({ roomId, roomMetadata }: CollaborativeRoomProps) => {
-  return (
-    <RoomProvider id={roomId}>
-        <ClientSideSuspense fallback={<div>Loading…</div>}>
-            <div className="collaborative-room">
-                <Header>
-                    <div className="flex w-fit items-center justify-center gap-2">
-                        <p className="document-title">Share</p>
-                    </div>
+    const currentUserType = 'editor'
 
-                    <div className="flex w-full flex-1 justify-end gap-2 sm:gap-3">
-                        <ActiveCollaborators />
+    const [documentTitle, setDocumentTitle] = useState(roomMetadata.title)
+    const [editing, setEditing] = useState(false)
+    const [loading, setLoading] = useState(false)
 
-                        <SignedOut>
-                            <SignInButton />
-                        </SignedOut>
-                        <SignedIn>
-                            <UserButton />
-                        </SignedIn>
-                    </div>
+    const containerRef = useRef<HTMLDivElement>(null)
+    const inputRef = useRef<HTMLDivElement>(null)
 
-                    
-                    
-                </Header>
-                <Editor />
+    const updateTitleHandler = (e: React.KeyboardEvent<HTMLInputElement>) => {
 
-            </div>
-        </ClientSideSuspense>
-    </RoomProvider>
-  )
+    }
+
+    return (
+        <RoomProvider id={roomId}>
+            <ClientSideSuspense fallback={<div>Loading…</div>}>
+                <div className="collaborative-room">
+                    <Header>
+                        <div ref={containerRef} className="flex w-fit items-center justify-center gap-2">
+                            {editing && !loading ? (
+                                <Input 
+                                    type="text"
+                                    value={documentTitle}
+                                    ref={inputRef}
+                                    placeholder="Enter title"
+                                    onChange={(e) => setDocumentTitle(e.target.value)}
+                                    onKeyDown={updateTitleHandler}
+                                    disabled={!editing}
+                                    className="document-title-input"
+                                />
+                            ) : (
+                                <>
+                                    <p className="document-title">{documentTitle}</p>
+                                </>
+                            )}
+
+                            { currentUserType === 'editor' && !editing && (
+                                <Image 
+                                    src="/assets/icons/edit.svg"
+                                    alt="edit"
+                                    width={24}
+                                    height={24}
+                                    onClick={() => setEditing(true)}
+                                    className="pointer"
+                                />
+                            )}
+
+                            { currentUserType !== 'editor' && !editing && (
+                                <p className="view-only-tag">
+                                    View Only
+                                </p>
+                            )}
+
+                            {loading && <p className="text-sm text-gray-400">saving...</p>}
+                        </div>
+
+                        <div className="flex w-full flex-1 justify-end gap-2 sm:gap-3">
+                            <ActiveCollaborators />
+
+                            <SignedOut>
+                                <SignInButton />
+                            </SignedOut>
+                            <SignedIn>
+                                <UserButton />
+                            </SignedIn>
+                        </div>
+
+
+
+                    </Header>
+                    <Editor />
+
+                </div>
+            </ClientSideSuspense>
+        </RoomProvider>
+    )
 }
 
 export default CollaborativeRoom
