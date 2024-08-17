df4d72feddd6ef6dc6351bcdc86cfe1470a84e67
Ranga McMil <mukanhairiranga@gmail.com>
2024-08-17 12:54:15 +0200

    room action

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,10 +1,21 @@
+import Header from "@/components/Header"
 import { Button } from "@/components/ui/button"
+import { SignedIn, UserButton } from "@clerk/nextjs"
 
 const Home = () => {
+  const documents = []
+  
   return (
-    <div>
-      <Button>Click this</Button> 
-    </div>
+    <main className="home-container">
+      <Header className="sticky left-0 top-0">
+        <div className="flex items-center gap-2 lg:gap-4">
+          Notification
+          <SignedIn>
+            <UserButton />
+          </SignedIn>
+        </div>
+      </Header>
+    </main>
   )
 }
 
