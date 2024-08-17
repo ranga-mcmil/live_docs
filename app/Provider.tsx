c4364e6bea51cbe5d398e3a8090885ddb36a68f3
Ranga McMil <mukanhairiranga@gmail.com>
2024-08-17 17:48:41 +0200

    get clerk users action

--- app/Provider.tsx
+++ app/Provider.tsx
@@ -1,17 +1,24 @@
 "use client"
 
 import Loader from "@/components/Loader";
+import { getClerkUsers } from "@/lib/actions/user.actions";
 import {
-    LiveblocksProvider,
-    ClientSideSuspense,
-  } from "@liveblocks/react/suspense";
+  LiveblocksProvider,
+  ClientSideSuspense,
+} from "@liveblocks/react/suspense";
 
 const Provider = ({ children }: { children: React.ReactNode }) => {
   return (
-    <LiveblocksProvider authEndpoint="/api/liveblocks-auth">
-        <ClientSideSuspense fallback={<Loader />}>
-            {children}
-        </ClientSideSuspense>
+    <LiveblocksProvider
+      authEndpoint="/api/liveblocks-auth"
+      resolveUsers={async ({userIds}) => {
+        const users = await getClerkUsers({userIds})
+        return users
+      }}
+    >
+      <ClientSideSuspense fallback={<Loader />}>
+        {children}
+      </ClientSideSuspense>
     </LiveblocksProvider>
   )
 }
--- components/CollaborativeRoom.tsx
+++ components/CollaborativeRoom.tsx
@@ -4,6 +4,7 @@ import { RoomProvider, ClientSideSuspense} from "@liveblocks/react/suspense";
 import { Editor } from "@/components/editor/Editor"
 import Header from "@/components/Header"
 import { SignInButton, SignedIn, SignedOut, UserButton } from '@clerk/nextjs'
+import ActiveCollaborators from "./ActiveCollaborators";
 
 const CollaborativeRoom = () => {
   return (
@@ -15,12 +16,18 @@ const CollaborativeRoom = () => {
                         <p className="document-title">Share</p>
                     </div>
 
-                    <SignedOut>
-                        <SignInButton />
-                    </SignedOut>
-                    <SignedIn>
-                        <UserButton />
-                    </SignedIn>
+                    <div className="flex w-full flex-1 justify-end gap-2 sm:gap-3">
+                        <ActiveCollaborators />
+
+                        <SignedOut>
+                            <SignInButton />
+                        </SignedOut>
+                        <SignedIn>
+                            <UserButton />
+                        </SignedIn>
+                    </div>
+
+                    
                     
                 </Header>
                 <Editor />
