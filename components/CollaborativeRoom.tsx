80b8a637a7539f925364c47706cb9b62069e07a3
Ranga McMil <mukanhairiranga@gmail.com>
2024-08-17 22:17:57 +0200

    floating comments

--- components/CollaborativeRoom.tsx
+++ components/CollaborativeRoom.tsx
@@ -11,9 +11,7 @@ import Image from "next/image";
 import { updateDocument } from "@/lib/actions/room.actions";
 import Loader from "./Loader";
 
-const CollaborativeRoom = ({ roomId, roomMetadata }: CollaborativeRoomProps) => {
-    const currentUserType = 'editor'
-
+const CollaborativeRoom = ({ roomId, roomMetadata, users, currentUserType }: CollaborativeRoomProps) => {
     const [documentTitle, setDocumentTitle] = useState(roomMetadata.title)
     const [editing, setEditing] = useState(false)
     const [loading, setLoading] = useState(false)
@@ -123,7 +121,7 @@ const CollaborativeRoom = ({ roomId, roomMetadata }: CollaborativeRoomProps) => 
 
 
                     </Header>
-                    <Editor />
+                    <Editor roomId={roomId} currentUserType={currentUserType}/>
 
                 </div>
             </ClientSideSuspense>
