91d69ba583be42b7df01d350f9e351aba00d0a3e
Ranga McMil <mukanhairiranga@gmail.com>
2024-08-17 11:01:35 +0200

    add live blocks

--- app/layout.tsx
+++ app/layout.tsx
@@ -8,6 +8,7 @@ import {
   ClerkProvider,
 } from '@clerk/nextjs'
 import { dark } from "@clerk/themes"
+import Provider from "./Provider"
 
 const fontSans = FontSans({
   subsets: ["latin"],
@@ -37,7 +38,9 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
             fontSans.variable
           )}
         >
-          {children}
+          <Provider>
+            {children}
+          </Provider>
         </body>
       </html>
     </ClerkProvider>
