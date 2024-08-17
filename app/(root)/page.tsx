577fdde0e3d939422b47bd66dc67d7f753a63b3c
Ranga McMil <mukanhairiranga@gmail.com>
2024-08-17 13:27:31 +0200

    homepage

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,8 +1,15 @@
+import AddDocumentBtn from "@/components/AddDocumentBtn"
 import Header from "@/components/Header"
 import { SignedIn, UserButton } from "@clerk/nextjs"
+import { currentUser } from "@clerk/nextjs/server"
 import Image from "next/image"
+import { redirect } from "next/navigation"
 
-const Home = () => {
+const Home = async () => {
+  const clerkUser = await currentUser()
+
+  if (!clerkUser) redirect('/sign-in')
+
   const documents = []
 
   return (
@@ -26,6 +33,11 @@ const Home = () => {
             width={40}
             height={40}
             className="mx-auto"
+          />
+
+          <AddDocumentBtn 
+            userId={clerkUser.id}
+            email={clerkUser.emailAddresses[0].emailAddress}
           />
         </div>
       )}
