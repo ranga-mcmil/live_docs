4697a1087b810a7202e0da4fd52fa94973e422e0
Ranga McMil <mukanhairiranga@gmail.com>
2024-08-19 10:03:16 +0200

    delete document

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,4 +1,5 @@
 import AddDocumentBtn from "@/components/AddDocumentBtn"
+import { DeleteModal } from "@/components/DeleteModal"
 import Header from "@/components/Header"
 import { getDocuments } from "@/lib/actions/room.actions"
 import { dateConverter } from "@/lib/utils"
@@ -56,7 +57,7 @@ const Home = async () => {
                   </div>
                 </Link>
 
-                {/* TODO: add delete button */}
+                <DeleteModal roomId={id}/>
               </li>
             ))}
 
--- lib/actions/room.actions.ts
+++ lib/actions/room.actions.ts
@@ -4,6 +4,7 @@ import { nanoid } from 'nanoid'
 import { revalidatePath } from 'next/cache';
 import { getAccessType, parseStringify } from '../utils';
 import { liveblocks } from '../liveblocks';
+import { redirect } from 'next/navigation';
 
 export const createDocument = async({ userId, email }: CreateDocumentParams) => {
     const roomId = nanoid()
@@ -117,5 +118,15 @@ export const removeCollaborator = async ({roomId, email}: { roomId: string, emai
         return parseStringify(updatedRoom)
     } catch (error) {
         throw new Error(`blahh blahh error removing a collaborator: ${error}`)
+    }
+}
+
+export const deleteDocument = async (roomId: string) => {
+    try {
+        await liveblocks.deleteRoom(roomId)
+        revalidatePath('/')
+        redirect('/')
+    } catch (error) {
+        throw new Error(`blahh blahh error deleting a document: ${error}`)
     }
 }
